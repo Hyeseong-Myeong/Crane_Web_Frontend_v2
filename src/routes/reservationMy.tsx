30d28e28fde9a94fff9dd8bc82bc810371109fe6
Hyeseong-Myeong <a01025819869@gmail.com>
2024-11-05 12:01:12 +0900

    [Impl] Implement MyReservation Page

--- src/routes/reservationMy.tsx
+++ src/routes/reservationMy.tsx
@@ -19,17 +19,35 @@ const Wrapper = styled.div`
     margin-top: 80px;
 `;
 
-const Reservation = styled.a`
+const Reservation = styled.a``;
 
-`;
-
-const ResContainer = styled.div`
-
-`;
+const ResContainer = styled.div``;
 
 const ResBlock = styled.div`
     margin: 10px;
     padding: 10px;
+    border-radius: 15px;
+    box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1), 0px 0px 6px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const UpperResBox = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 10px;
+    font-size: 20px;
+    font-weight: 500;
+`;
+
+const ResCancelButton = styled.button`
+    border: none;
+    padding: 5px;
+    cursor: pointer;
+`;
+
+const LowwerResBox = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 10px;
 `;
 
 export default function ReservationMy() {
@@ -49,6 +67,7 @@ export default function ReservationMy() {
                 console.log(err);
             }
         };
+
         const fetchInst = async () => {
             try {
                 const res = await axios.get(
@@ -66,21 +85,22 @@ export default function ReservationMy() {
     }, []);
 
     useEffect(() => {
-        const fetchResList = async () => {
-            if (uid !== null) {
-                try {
-                    const res = await axios.get(
-                        `${import.meta.env.VITE_API_URL}/reservation/user/${uid}`,
-                        { withCredentials: true }
-                    );
-                    setResList(res.data); 
-                } catch (err) {
-                    console.log(err);
-                }
-            }
-        };
         fetchResList();
     }, [uid]);
+
+    const fetchResList = async () => {
+        if (uid !== null) {
+            try {
+                const res = await axios.get(
+                    `${import.meta.env.VITE_API_URL}/reservation/user/${uid}`,
+                    { withCredentials: true }
+                );
+                setResList(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    };
 
     const getStatusLabel = (status: string | null) => {
         switch (status) {
@@ -95,6 +115,25 @@ export default function ReservationMy() {
         }
     };
 
+    const onClickCancelRes = (rid: number) => {
+        if (window.confirm("정말 취소하시겠습니까?")) {
+            const cancelRes = async () => {
+                try {
+                    const res = await axios.get(
+                        `${import.meta.env.VITE_API_URL}/reservation/${rid}/cancel`,
+                        { withCredentials: true }
+                    );
+                    if (res.status === 200) {
+                        window.location.reload()                    
+                    }
+                } catch (err) {
+                    console.log(err);
+                }
+            };
+            cancelRes();
+        }
+    };
+
     return (
         <Wrapper>
             <Reservation href="/reservation"> &lt; 예약 바로가기</Reservation>
@@ -103,20 +142,28 @@ export default function ReservationMy() {
                     const instrument = instList.find(inst => inst.iid === reservation.iid);
                     return (
                         <ResBlock key={reservation.rid}>
-                            <p>예약 장비: {instrument ? instrument.instName : "장비 정보 없음"}</p>
-                            <p>
-                                예약 시작 시간:{" "}
-                                {new Date(reservation.resStartTime).toLocaleDateString('ko-KR', {
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric',
-                                })}{" "}
-                                {new Date(reservation.resStartTime).toLocaleTimeString('ko-KR', {
-                                    hour: 'numeric',
-                                    minute: 'numeric',
-                                })}
-                            </p>
-                            <p>예약 상태: {getStatusLabel(reservation.reservationStatus)}</p>
+                            <UpperResBox>
+                                {instrument ? instrument.instName : "장비 정보 없음"}
+                                <ResCancelButton
+                                    onClick={() => onClickCancelRes(reservation.rid)}
+                                >
+                                    예약 취소
+                                </ResCancelButton>
+                            </UpperResBox>
+                            <LowwerResBox>
+                                <p>
+                                    {new Date(reservation.resStartTime).toLocaleDateString('ko-KR', {
+                                        year: 'numeric',
+                                        month: 'long',
+                                        day: 'numeric',
+                                    })}{" "}
+                                    {new Date(reservation.resStartTime).toLocaleTimeString('ko-KR', {
+                                        hour: 'numeric',
+                                        minute: 'numeric',
+                                    })}
+                                </p>
+                                {getStatusLabel(reservation.reservationStatus)}
+                            </LowwerResBox>
                         </ResBlock>
                     );
                 })}
